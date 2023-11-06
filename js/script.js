7416adbebf6a197c1831cf000b608946039c2bd9
Lagergrenk <k.lagergren@live.se>
2023-11-06 21:13:16 +0100

    Update script.js
    Updated "rad"
    Updated lists with numbers

---
+++ js/script.js
@@ -0,0 +1,61 @@
+const rowNumbers = ["Rad 1", "Rad 2", "Rad 3", "Rad 4", "Rad 5"];
+const fontSizes = ["x-small", "small", "medium", "large", "x-large"];
+const fontColor = "hsl(233, 70%, 75%)";
+
+for (let i = 0; i < rowNumbers.length; i++) {
+  // Creating h1 elements
+  let h1El = document.createElement("h1");
+
+  // Adding text i++
+  h1El.innerText = rowNumbers[i];
+
+  // Changing fontsize
+  h1El.style.fontSize = fontSizes[i];
+
+  // Chaning background
+  h1El.style.backgroundColor = `hsl(140, 100%, 56%)`;
+
+  //Changing text color
+  h1El.style.color = fontColor;
+  //Justify content to center
+  h1El.style.display = "flex";
+  h1El.style.justifyContent = "center";
+
+  // Appending to body
+  document.body.appendChild(h1El);
+}
+
+// Creating div element to house lists
+let divEl = document.createElement("div");
+divEl.style.border = "1px solid black";
+divEl.style.display = "flex";
+divEl.style.justifyContent = "space-around";
+document.body.appendChild(divEl);
+
+// Array with numbers in writing
+const numbersInWriting = [
+  "ett",
+  "två",
+  "tre",
+  "fyra",
+  "fem",
+  "sex",
+  "sju",
+  "åtta",
+  "nio",
+  "tio",
+];
+
+for (let i = 0; i <= 2; i++) {
+  let listEl = document.createElement("ul");
+  listEl.style.backgroundColor = fontColor;
+  listEl.style.listStyle = "none";
+  divEl.appendChild(listEl);
+  for (let j = 0; j <= 9; j++) {
+    let listItemEl = document.createElement("li");
+    if ((i = 2)) {
+      listItemEl.textContent = numbersInWriting[j];
+    }
+    listEl.appendChild(listItemEl);
+  }
+}
