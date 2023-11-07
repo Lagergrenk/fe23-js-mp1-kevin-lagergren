9c805da47b9b92ef2381a9746db50c4ad456ae2a
Lagergrenk <k.lagergren@live.se>
2023-11-07 09:38:23 +0100

    Change variable name and created counter for color

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const rowNumbers = ["Rad 1", "Rad 2", "Rad 3", "Rad 4", "Rad 5"];
 const fontSizes = ["x-small", "small", "medium", "large", "x-large"];
-const fontColor = "hsl(233, 70%, 75%)";
+const fontAndBCGColor = "hsl(233, 70%, 75%)";
+let colorCounter = 140;
 
 for (let i = 0; i < rowNumbers.length; i++) {
   // Creating h1 elements
@@ -13,10 +14,11 @@ for (let i = 0; i < rowNumbers.length; i++) {
   h1El.style.fontSize = fontSizes[i];
 
   // Chaning background
-  h1El.style.backgroundColor = `hsl(140, 100%, 56%)`;
+  h1El.style.backgroundColor = `hsl(${colorCounter}, 100%, 55%)`;
+  colorCounter += 10;
 
   //Changing text color
-  h1El.style.color = fontColor;
+  h1El.style.color = fontAndBCGColor;
   //Justify content to center
   h1El.style.display = "flex";
   h1El.style.justifyContent = "center";
@@ -51,17 +53,28 @@ const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 // loop to create outer lists
 for (let i = 0; i <= 2; i++) {
   let listEl = document.createElement("ul");
-  listEl.style.backgroundColor = fontColor;
+  listEl.style.backgroundColor = fontAndBCGColor;
   listEl.style.listStyle = "none";
+  listEl.style.margin = "10px";
+  listEl.style.display ="flex";
+  listEl.style.flexDirection ="column";
   divEl.appendChild(listEl);
 
   // inner loop to create inner listitems
   for (let j = 0; j <= 9; j++) {
     let listItemEl = document.createElement("li");
     listItemEl.style.margin = "5px";
+    
 
     if (i == 0) {
       listItemEl.textContent = j;
+      listEl.style.justifyContent = "flex-start";
+    }
+
+    if(i == 1){
+      listItemEl.textContent = j;
+      listEl.style.flexDirection = "column-reverse";
+
     }
 
     if (i == 2) {
